3c033da4eef2a1e61e026c82327dbd28cc9f8b5e
XL1TTE <zzzxl1tteqqq@mail.ru>
2024-04-15 19:26:36 +0700

    Navigation service rework

---
+++ Project XLT/MVVM/ViewModels/GeneralViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Project_XLT.MVVM.Core;
+using Project_XLT.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project_XLT.MVVM.ViewModels
+{
+    public class GeneralViewModel: ViewModelBase
+    {
+        private InavigationService _navigation;
+        public InavigationService Navigation
+        {
+            get => _navigation;
+            set
+            {
+                _navigation = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public RelayCommand NavigateToNutrition { get; set; }
+        public RelayCommand NavigateToMainMenu { get; set; }
+        public RelayCommand ExitCommand { get; set; }
+        public GeneralViewModel(InavigationService navigation)
+        {
+            Navigation = navigation;
+            Navigation.LocalNavigateTo<MainMenuViewModel>();
+            NavigateToNutrition = new RelayCommand(o => { Navigation.LocalNavigateTo<NutritionViewModel>(); }, o => true);
+            NavigateToMainMenu = new RelayCommand(o => { Navigation.LocalNavigateTo<MainMenuViewModel>(); }, o => true);
+            ExitCommand = new RelayCommand(o => { Application.Current.Shutdown(); }, o => true);
+        }
+    }
+}
+
