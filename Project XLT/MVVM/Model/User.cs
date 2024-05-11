13e67a92fae3097243c7249290d41444bbd6f945
XL1TTE <zzzxl1tteqqq@mail.ru>
2024-05-11 16:42:08 +0700

    f-7

---
+++ Project XLT/MVVM/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_XLT.MVVM.Model
+{
+    public class User
+    {
+        public Dictionary<string, UserData> UserDataByTime { get; set; }
+    }
+}
